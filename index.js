f91d3fd8c5c5f7532c74ebedb3acb840700b31ac
Md Ridwan <72821054+mdridwwan@users.noreply.github.com>
2021-07-31 21:31:41 +0600

    Last Practices

---
+++ index.js
@@ -0,0 +1,36 @@
+var age = 13;
+var onionAge = 34;
+var allPrice = age / onionAge;
+console.log(allPrice);
+
+
+// mathematical operation
+var price1 = 31;
+var price2 = 10;
+var difference = price2 - price1;
+price1 -= 10;
+var age = 20;
+age = age - 1;
+age -= 1
+age--
+console.log(difference);
+console.log(price1);
+console.log(age);
+var first = 'Md';
+var last = 'Ridwan'
+var name = first + ' ' + last;
+console.log(typeof name);
+var onionPrice = '20.5';
+var eggPrice = '30.33';
+var onionPriceNumber = parseFloat(onionPrice);
+var eggPriceNumber = parseInt(eggPrice);
+console.log(onionPriceNumber)
+console.log(typeof eggPriceNumber);
+var today = new Date();
+console.log(typeof today);
+var rPrice = 0.1;
+var tPrice = 0.2;
+var total = rPrice / tPrice;
+total = total.toFixed(1);
+total = parseFloat(total);
+console.log(17 % 5);
