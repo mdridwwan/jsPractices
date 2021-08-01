bf895b7f616617c30013586259b76eeda715b8bf
Md Ridwan <72821054+mdridwwan@users.noreply.github.com>
2021-08-01 17:28:13 +0600

    last update

--- comparison.js
+++ comparison.js
@@ -5,3 +5,54 @@
 // } else {
 //     console.log('amr kase porjabto porimane tk nai go')
 // }
+// var myJob = false;
+// var myMoney = 20000;
+// var myFlat = false;
+// if (myJob == true || myMoney > 200000 || myFlat == true) {
+//     console.log('cholo amra biye kory feli')
+// } else {
+//     console.log('amr kopaly biye nai')
+// }
+// // answer: amr kopaly biye nai
+
+// var myJob = false;
+// var myMoney = 20000;
+// var myHouse = true;
+// if ((myJob == true && myMoney > 10000 || myHouse == true)) {
+//     console.log('cholo biye kory feli')
+// } else {
+//     console.log('amr kopale biye nai')
+// }
+// answer{cholo biye kory feli}
+
+// var danishPrice = 520;
+// var biscuitPrice = 205;
+// var batterBonPrice = 100;
+// var myBudget = 100;
+// if (danishPrice < myBudget) {
+//     console.log('danish kheye dhaka chole jabu');
+// } else if (biscuitPrice < myBudget) {
+//     console.log('biscuit kheye noakhali chole jabu')
+// } else if (batterBonPrice < myBudget) {
+//     console.log('biscuit kheye Pakistan jabu')
+// } else {
+//     console.log('batasha kheye Swichtland jabu');
+// }
+// answer {batasha kheye Swichtland jabu}
+
+// nesten
+
+// var arr = [3, 7, 10];
+// arr = arr[1];
+// console.log(arr);
+// anser{7}
+
+// var arr = [1, 2, 4, 5, ]
+// arr.shift();
+// console.log(arr);
+// first element remove
+// answer{[ 2, 4, 5 ]}
+
+// var nums = [1, 5, 8];
+// nums = nums.indexOf(1)
+console.log(6 >= 6);
